faa68d0fc3e85502449b8f143ffca9510521da22
Philip Geurin <philip.geurin@gmail.com>
2018-05-23 23:25:05 -0700

    add merge dataframe to run_all

--- src/run_all.py
+++ src/run_all.py
@@ -1,38 +1,18 @@
 import pandas as pd
 from sessions_analysis_and_plot import sessions_to_plots
 from posts_analysis_and_plot import posts_to_plots
-
-
-def import_all():
-    teachers = pd.read_csv('../data_january/teachers.csv')
-    students = pd.read_csv('../data_january/students.csv')
-    schools = pd.read_csv('../data_january/schools.csv')
-    posts = pd.read_csv('../data_january/posts.csv')
-    posts['exists']=1
-    posts=posts.rename(columns={'id':'post_id'})
-    posts['date'] = pd.to_datetime(posts.date, errors='coerce')
-    planning_events = pd.read_csv('../data_january/planning_events.csv')
-    child_posts = pd.read_csv('../data_january/child_posts.csv')
-    lesson_posts = pd.read_csv('../data_january/lesson_posts.csv')
-    parents = pd.read_csv('../data_january/parents.csv')
-    sessions = pd.read_csv('../data_january/sessions.csv')
-    sessions['start_date'] = pd.to_datetime(sessions['start_date'])
-    sessions['stop_date'] = pd.to_datetime(sessions['stop_date'])
-    sessions['exists']=1
-    classrooms = pd.read_csv('../data_january/classrooms.csv')
-    classrooms=classrooms.rename(columns={'id':'classroom_id'})
-    # posts['date'] = pd.to_datetime(posts.date, errors='coerce')
-    classrooms_merged = pd.read_csv('../data_january/classrooms_merged.csv')
-    return teachers, students, classrooms, sessions, schools, posts, planning_events, child_posts, lesson_posts, parents, classrooms_merged
-(teachers, students, classrooms, sessions, schools, posts, planning_events,
- child_posts, lesson_posts, parents, classrooms_merged) = import_all()
-
+from merge_dataframe_leaky import make_classrooms_merged
+from data_load import load_all
 
 def main():
     (teachers, students, classrooms, sessions, schools, posts, planning_events,
-     child_posts, lesson_posts, parents, classrooms_merged) = import_all()
+     child_posts, lesson_posts, parents, classrooms_merged) = load_all()
     sessions_to_plots(sessions)
     posts_to_plots(posts)
+    classrooms_merged_all_leaky = make_classrooms_merged(classrooms, posts,
+                                                         teachers, students,
+                                                         lesson_posts)
+
 
 if __name__ == "__main__":
     main()
